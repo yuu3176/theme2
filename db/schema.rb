9a66ed1eab7125a320a6faac89e66169a71a1939
yuu3176 <129866049+yuu3176@users.noreply.github.com>
2023-05-13 09:54:02 +0000

    [finish]3-12

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2023_04_29_185346) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
